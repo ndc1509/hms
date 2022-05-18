4c76eb1630383e09ed3e018a946f3670575a215a
ndc1509 <78099549+ndc1509@users.noreply.github.com>
2022-05-18 15:58:57 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import FloorPlan from "./components/rooms/FloorPlan";
 import socketIOClient from "socket.io-client";
 import { waitForCheckOut } from "./store/reducers/roomsSlice";
 function App() {
-    const host = "https://cannonfort.vercel.app";
+    const host = "https://cannonfort.herokuapp.com";
 
     const dispatch = useDispatch();
     useEffect(() => {
