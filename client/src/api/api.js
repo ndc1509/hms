3e24537644e44023c09251f9d4088f0cf3c61f46
ndc1509 <nguyendinhcuonga12@gmail.com>
2022-05-18 14:37:03 +0700

    fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,14 +2,15 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { ROOM_STATUS } from "../constants";
 
+const host = 'https://cannonfort.vercel.app'
 export const getRooms = createAsyncThunk("getRooms", async () => {
-    const res = await axios.get("http://192.168.1.45:5000/api/rooms");
+    const res = await axios.get(`${host}/api/rooms`);
     return res.data.rooms;
 });
 
 export const checkIn = createAsyncThunk("checkIn", async (guest) => {
     const res = await axios.post(
-        "http://192.168.1.45:5000/api/guests/check-in",
+        `${host}/api/guests/check-in`,
         guest
     );
     return res.data.guest;
@@ -18,7 +19,7 @@ export const checkIn = createAsyncThunk("checkIn", async (guest) => {
 export const checkOut = createAsyncThunk("checkOut", async (guest) => {
     console.log(typeof guest._id);
     const res = await axios.post(
-        "http://192.168.1.45:5000/api/guests/check-out",
+       `${host}/api/guests/check-out`,
         { _id: guest._id }
     );
     return res.data.guest;
@@ -26,13 +27,13 @@ export const checkOut = createAsyncThunk("checkOut", async (guest) => {
 
 export const cleaned = createAsyncThunk("cleaned", async (room) => {
     const res = await axios.put(
-        `http://192.168.1.45:5000/api/rooms/cleaned/${room.id}`,
+        `${host}/api/rooms/cleaned/${room.id}`,
         { status: ROOM_STATUS.CLEAN }
     );
     return res.data.room;
 });
 
 export const getGuests = createAsyncThunk("getGuests", async () => {
-    const res = await axios.get(`http://192.168.1.45:5000/api/guests`);
+    const res = await axios.get(`${host}/api/guests`);
     return res.data.guests;
 });
