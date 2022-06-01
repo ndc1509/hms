5099832919aebe8e211930b19b7e282ec93cd1bc
ndc1509 <78099549+ndc1509@users.noreply.github.com>
2022-06-01 11:27:02 +0700

    Update axiosBase.js

--- client/src/api/axiosBase.js
+++ client/src/api/axiosBase.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 const axiosBase = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://cannonfort.herokuapp.com/api",
     headers: {
         "Content-type": "application/json",
     },
